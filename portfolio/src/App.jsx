b5d69afe9559bfb66446546e342aa166341ca612
infesto05 <aasifmohamed32@gmail.com>
2026-03-21 22:32:48 +0530

    fix all import casting issues

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -7,9 +7,9 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Navbar from "./components/Navbar";
 import Transition from "./components/Transition";
-import Projects from "./components/projects";
+import Projects from "./components/Projects";
 import Skills from "./components/Skills";
-import Certifications from "./components/certifications";
+import Certifications from "./components/Certifications";
 import Contact from "./components/Contact";
 
 function App() {
